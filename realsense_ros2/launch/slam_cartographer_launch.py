140843de88203a27fb14270f5df811bcd77e44fb
Juan Galvis <jdgalviss@gmail.com>
2020-12-26 18:42:41 +0400

    feat: navigation stack running

--- realsense_ros2/launch/slam_cartographer_launch.py
+++ realsense_ros2/launch/slam_cartographer_launch.py
@@ -87,29 +87,29 @@ def generate_launch_description():
                         ('depth_camera_info', 'rs_d435/aligned_depth/camera_info')],
             ),
 
-        Node(
-            package='cartographer_ros',
-            node_executable='cartographer_node',
-            output='log',
-            parameters=[{'use_sim_time': use_sim_time}],
-            arguments=['-configuration_directory', cartographer_config_dir, '-configuration_basename', configuration_basename],
-            remappings=[('odom','rs_t265/odom'),
-            ('imu','rs_t265/imu')]),
+#        Node(
+#            package='cartographer_ros',
+#            node_executable='cartographer_node',
+#            output='log',
+#            parameters=[{'use_sim_time': use_sim_time}],
+##            arguments=['-configuration_directory', cartographer_config_dir, '-configuration_basename', configuration_basename],
+#            remappings=[('odom','rs_t265/odom'),
+#            ('imu','rs_t265/imu')]),
 
-         DeclareLaunchArgument(
-            'resolution',
-            default_value=resolution,
-            description='Resolution of a grid cell in the published occupancy grid'),
+#         DeclareLaunchArgument(
+#            'resolution',
+#            default_value=resolution,
+#            description='Resolution of a grid cell in the published occupancy grid'),
 
-        DeclareLaunchArgument(
-            'publish_period_sec',
-            default_value=publish_period_sec,
-            description='OccupancyGrid publishing period'),
+#        DeclareLaunchArgument(
+#            'publish_period_sec',
+#            default_value=publish_period_sec,
+#            description='OccupancyGrid publishing period'),
 
-        Node(
-            package='cartographer_ros',
-            node_executable='occupancy_grid_node',
-            node_name='occupancy_grid_node',
-            parameters=[{'use_sim_time': use_sim_time}],
-            arguments=['-resolution', resolution, '-publish_period_sec', publish_period_sec]),
+#        Node(
+#            package='cartographer_ros',
+#            node_executable='occupancy_grid_node',
+#            node_name='occupancy_grid_node',
+#            parameters=[{'use_sim_time': use_sim_time}],
+#            arguments=['-resolution', resolution, '-publish_period_sec', publish_period_sec]),
     ])
