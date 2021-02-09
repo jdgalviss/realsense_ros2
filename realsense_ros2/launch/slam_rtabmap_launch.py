bd5d386f04361da82a0cbfee1c2a9bbeea27d7d4
Juan Galvis <jdgalviss@gmail.com>
2021-02-09 23:18:28 +0400

    fix: use_sim_time slam_rtabmap_launch

--- realsense_ros2/launch/slam_rtabmap_launch.py
+++ realsense_ros2/launch/slam_rtabmap_launch.py
@@ -7,7 +7,7 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 
 def generate_launch_description():
-    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
 
     parameters=[{
           'queue_size':20,
